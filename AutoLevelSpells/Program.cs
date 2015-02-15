b411ba3d8ea5ed82da161a511eed8e244657829a
h4mn <h4mn@joduska.me>
2015-02-15 21:26:37 -0200

    Teemo priorize 'Q'

--- AutoLevelSpells/Program.cs
+++ AutoLevelSpells/Program.cs
@@ -331,7 +331,8 @@ namespace AutoLevelSpells
                     sequence = new[] { 3, 2, 1, 2, 2, 4, 1, 2, 2, 1, 4, 1, 1, 3, 3, 4, 3, 3 };
                     break;
                 case "Teemo":
-                    sequence = new[] { 1, 3, 2, 3, 1, 4, 3, 3, 3, 1, 4, 2, 2, 1, 2, 4, 2, 1 };
+                    //sequence = new[] { 1, 3, 2, 3, 1, 4, 3, 3, 3, 1, 4, 2, 2, 1, 2, 4, 2, 1 };
+                    sequence = new[] { 1, 3, 1, 3, 2, 4, 1, 1, 3, 1, 4, 3, 3, 2, 2, 4, 2, 2 };
                     break;
                 case "Thresh":
                     sequence = new[] { 1, 3, 2, 2, 2, 4, 2, 3, 2, 3, 4, 3, 3, 1, 1, 4, 1, 1 };
